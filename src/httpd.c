6a1122b50b94f8fdb5d601c137c20e0c5977a5d5
Christian A. Jacobsen <c.a.jacobsen95@gmail.com>
2017-11-05 16:12:53 +0000

    Smalli smalli erroritas

--- src/httpd.c
+++ src/httpd.c
@@ -1656,12 +1656,12 @@ void configure_context(SSL_CTX *ctx)
     SSL_CTX_set_ecdh_auto(ctx, 1);
 
     // Set the key and cert
-    if (SSL_CTX_use_certificate_file(ctx, "../cert.pem", SSL_FILETYPE_PEM) <= 0) {
+    if (SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM) <= 0) {
         ERR_print_errors_fp(stderr);
 	    exit(1);
     }
 
-    if (SSL_CTX_use_PrivateKey_file(ctx, "../key.pem", SSL_FILETYPE_PEM) <= 0 ) {
+    if (SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM) <= 0 ) {
         ERR_print_errors_fp(stderr);
 	    exit(1);
     }
